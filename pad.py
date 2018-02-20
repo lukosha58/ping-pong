933e9a985a7a729356c2a8c20cb325a7ac4fa99b
Alexey <lukosha58@yandex.ru>
2018-02-20 11:49:35 +0300

    Changed the speed

--- pad.py
+++ pad.py
@@ -2,15 +2,16 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, w, h,group):
+    def __init__(self, w, h, group):
+        self.border = (0, 15, w - 15, h - 25)
         self.x = 0
         self.y = h // 2 - 50
         super().__init__(group)
         self.image = pygame.Surface([15, 150])
+        self.image.fill(pygame.Color('blue'))
         self.rect = pygame.Rect(self.x, self.y, 15, 150)
         self.v = 5
         self.score = 0
-        pygame.draw.rect(self.image, pygame.Color('purple'), self.rect)
 
     def update(self):
         pressed = pygame.key.get_pressed()
@@ -18,19 +19,20 @@ class Player(pygame.sprite.Sprite):
             self.rect.y -= self.v
         if pressed[pygame.K_s]:
             self.rect.y += self.v
-        self.rect.clamp_ip(pygame.display.get_surface().get_rect())
+        self.rect.clamp_ip(self.border)
 
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, w, h, group):
+        self.border = (15, 15, w - 15, h - 25)
         self.x = w - 15
         self.y = h // 2 - 50
         super().__init__(group)
         self.image = pygame.Surface([15, 150])
+        self.image.fill(pygame.Color('red'))
         self.rect = pygame.Rect(self.x, self.y, 15, 150)
         self.score = 0
         self.v = 5
-        pygame.draw.rect(self.image, pygame.Color('purple'), self.rect)
 
     def update(self):
         pressed = pygame.key.get_pressed()
@@ -38,4 +40,4 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.y -= self.v
         if pressed[pygame.K_DOWN]:
             self.rect.y += self.v
-        self.rect.clamp_ip(pygame.display.get_surface().get_rect())
+        self.rect.clamp_ip(self.border)
